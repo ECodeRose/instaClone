9f7d431e61e9c02f093f161724f99ff157a47097
Ethan Rose <ethanrose.er@googlemail.com>
2024-03-11 15:56:36 +0000

    enhancement: sidebar complete with logout

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -2,6 +2,7 @@ import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 
+// instead of adding the sidebar component to every page, I added it only once to the PageLayout component and wrapped the children with it. This way, the sidebar will be displayed on every page except for the /auth page.
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
   return (
