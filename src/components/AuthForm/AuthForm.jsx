e78f9be72586bada06a5b89c71438f20fecca288
Ethan Rose <ethanrose.er@googlemail.com>
2024-03-11 10:54:08 +0000

    enhancement: added alternation between login and sign up

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -45,7 +45,32 @@ const AuthForm = () => {
             </Text>
             <Box flex={2} h={"1px"} bg={"gray.400"} />
           </Flex>
+
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            cursor={"pointer"}
+          >
+            <Image src="/google.png" w={5} alt="Google logo" />
+            <Text mx="2" color={"blue.500"}>
+              Log in with Google
+            </Text>
+          </Flex>
         </VStack>
+      </Box>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </Box>
+          <Box
+            onClick={() => setIsLogin(!isLogin)}
+            color={"blue.500"}
+            cursor={"pointer"}
+          >
+            {isLogin ? "Sign up" : "Log in"}
+          </Box>
+        </Flex>
       </Box>
     </>
   );
