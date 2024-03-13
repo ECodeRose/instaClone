d18ae126c6796ee96a2b7e1f1a0f5e8c38d0c88b
Ethan Rose <ethanrose.er@googlemail.com>
2024-03-13 00:54:27 +0000

    enhancement: added PostHeader and edited HomePage layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import { Box, Container, Flex } from "@chakra-ui/react";
+import FeedPosts from "../../components/FeedPosts/FeedPosts";
 
 const HomePage = () => {
   return (
     <Container maxW={"container.lg"}>
       <Flex gap={20}>
-        <Box flex={2} py={10} border={"1px solid blue"}>
+        <Box flex={2} py={10}>
           <FeedPosts />
         </Box>
         <Box
@@ -12,7 +13,6 @@ const HomePage = () => {
           mr={20}
           display={{ base: "none", lg: "block" }}
           maxW={"300px"}
-          border={"1px solid red"}
         >
           Suggested Users
         </Box>
