7154e8535da2c53a2d8f8b404b0a1965b6136a0d
Ethan Rose <ethanrose.er@googlemail.com>
2024-03-13 02:07:51 +0000

    enhancement: props passed into PostHeader and PostFooter

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -14,7 +14,7 @@ import {
   UnlikeLogo,
 } from "../../assets/constants";
 
-const PostFooter = () => {
+const PostFooter = ({ username }) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -43,7 +43,7 @@ const PostFooter = () => {
         {likes} likes
       </Text>
       <Text fontSize="sm" fontWeight={700}>
-        asaprogrammer_{" "}
+        {username}{" "}
         <Text as="span" fontWeight={400}>
           Feeling good
         </Text>
